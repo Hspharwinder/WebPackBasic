49e1cf2a68673e82244516f1082dc8d4279e0317
HSP <40883035+Hspharwinder@users.noreply.github.com>
2019-12-06 13:59:16 +0530

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,54 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
-module.exports =  {
-    entry: './app/index.js',
-    output:{ 
-        path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.js'
-    }
-}
+var DIST_DIR = path.resolve(__dirname, "dist"); // copy everything into dist folder
+var SRC_DIR = path.resolve(__dirname, "app"); // where to find untransfile src code 
+
+module.exports = {
+    mode: 'development',
+        entry: './src/index.js', // SRC_DIR + '\app/\index.js',
+        output:{ 
+            path: DIST_DIR, //also write like this path.resolve(__dirname, "dist"),
+            filename: 'bundle.js'
+            // publicPath: "/app/"
+        },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,    // all js files
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader" // babel-loader for transfile files
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
+
+/** with localhost  npm i webpack-dev-server */
+//  "scripts": {
+// "start": "webpack-dev-server --mode development --open --hot --port 6000",  by default port 8080
+//     "build": "webpack --mode production"
+//   }, 
+
+/** without localhost */
+//  "scripts": {
+// "start": "webpack -w", 
+//     "build": "webpack --mode production"
+//   },
+
+// https://blog.usejournal.com/setting-up-react-webpack-4-babel-7-from-scratch-2019-b771dca2f637
